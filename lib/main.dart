4d232dc6b48b8bbd9cca7cd40b4cb92328c328fc
Felix Maurer <Feeelix92@users.noreply.github.com>
2022-10-25 17:42:16 +0200

    added sensors_plus for demonstration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
 
@@ -36,6 +38,13 @@ class _MyHomePageState extends State<MyHomePage> {
   var _altitude = "";
   var _speed = "";
   var _address = "";
+  final Color _backgroundColor = Colors.white;
+  // accelerometer
+  final Map _userAccerlerometer = {'x': 0, 'y': 0, 'z': 0};
+  // gyroscope
+  final Map _gyroscope = {'x': 0, 'y': 0, 'z': 0};
+  // magnetometer
+  final Map _magnetometer = {'x': 0, 'y': 0, 'z': 0};
 
   Future<void> _updatePosition() async {
     Position pos = await _determinePosition();
@@ -70,8 +79,36 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _updatePosition();
+    userAccelerometerEvents.listen((UserAccelerometerEvent event) {
+      setState(() {
+        _userAccerlerometer['x'] = event.x;
+        _userAccerlerometer['y'] = event.y;
+        _userAccerlerometer['z'] = event.z;
+      });
+    });
+    gyroscopeEvents.listen((GyroscopeEvent event) {
+      setState(() {
+        _gyroscope['x'] = event.x;
+        _gyroscope['y'] = event.y;
+        _gyroscope['z'] = event.z;
+      });
+    });
+    magnetometerEvents.listen((MagnetometerEvent event) {
+      setState(() {
+        _magnetometer['x'] = event.x;
+        _magnetometer['y'] = event.y;
+        _magnetometer['z'] = event.z;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: _backgroundColor,
       appBar: AppBar(
         title: Text(widget.title),
       ),
@@ -82,7 +119,7 @@ class _MyHomePageState extends State<MyHomePage> {
             const Padding(
                 padding: EdgeInsets.all(16.0),
                 child: Text(
-                    'Your last know location is: ',
+                    'GPS: ',
                   style: TextStyle(
                     fontSize: 20,
                     color: Colors.green,
@@ -105,16 +142,82 @@ class _MyHomePageState extends State<MyHomePage> {
               'Speed: $_speed',
               style: Theme.of(context).textTheme.headline6,
             ),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                  'UserAccelerometer: ',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.blue,
+                  )
+              ),
+            ),
+            Text(
+              'x: ${_userAccerlerometer['x']}',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Text(
+              'y: ${_userAccerlerometer['y']}',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Text(
+              'z: ${_userAccerlerometer['z']}',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                  'Gyroscope: ',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.purple,
+                  )
+              ),
+            ),
+            Text(
+              'x: ${_gyroscope['x']}',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Text(
+              'y: ${_gyroscope['y']}',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Text(
+              'z: ${_gyroscope['z']}',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                  'Magnetometer: ',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.red,
+                  )
+              ),
+            ),
+            Text(
+              'x: ${_magnetometer['x']}',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Text(
+              'y: ${_magnetometer['y']}',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Text(
+              'z: ${_magnetometer['z']}',
+              style: Theme.of(context).textTheme.headline6,
+            ),
             // const Text('Address: '),
             //   Text(_address),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _updatePosition,
-        tooltip: 'GET GPS position',
-        child: const Icon(Icons.change_circle_outlined),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _updatePosition,
+      //   tooltip: 'GET GPS position',
+      //   child: const Icon(Icons.change_circle_outlined),
+      // ),
     );
   }
 }
