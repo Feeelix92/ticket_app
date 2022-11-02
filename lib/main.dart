f12fc3ccf48b1bd3202d1f0e19c502983fd2b7d3
Pascal Block <mail@pascalblock.de>
2022-11-02 11:29:25 +0100

    test background locator 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'dart:io';
+import 'dart:isolate';
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:background_locator_2/location_dto.dart';
+import 'package:background_locator_2/settings/ios_settings.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:background_locator_2/background_locator.dart';
+import 'package:app_settings/app_settings.dart';
 
 
 void main() {
@@ -67,13 +78,57 @@ class _QRCodeState extends State<QRCode> {
   }
 }
 
-
 class _MyHomePageState extends State<MyHomePage> {
   var _latitude = "";
   var _longitude = "";
   var _altitude = "";
   var _speed = "";
   var _address = "";
+
+  /////////////
+  static const String _isolateName = "LocatorIsolate";
+  ReceivePort port = ReceivePort();
+
+  get data => null;
+
+  @override
+  void initState() {
+    super.initState();
+
+    IsolateNameServer.registerPortWithName(port.sendPort, _isolateName);
+    port.listen((dynamic data) {
+      // do something with data
+    });
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    await BackgroundLocator.initialize();
+  }
+
+  @pragma('vm:entry-point')
+  static void callback(LocationDto locationDto) async {
+    final SendPort? send = IsolateNameServer.lookupPortByName(_isolateName);
+    send?.send(locationDto);
+  }
+
+  void startLocationService(){
+    BackgroundLocator.registerLocationUpdate(LocationCallbackHandler.callback,
+        initCallback: LocationCallbackHandler.initCallback,
+        initDataCallback: data,
+        disposeCallback: LocationCallbackHandler.disposeCallback,
+        autoStop: false,
+        iosSettings: IOSSettings(
+            accuracy: LocationAccuracy.bestForNavigation, distanceFilter: 0),
+        androidSettings: AndroidSettings(
+            accuracy: LocationAccuracy.bestForNavigation,
+            interval: 5,
+            distanceFilter: 0,
+            );
+  }
+
+  ////////
+
 
   Future<void> _updatePosition() async {
     Position pos = await _determinePosition();
@@ -127,6 +182,12 @@ class _MyHomePageState extends State<MyHomePage> {
                   )
                 ),
             ),
+            OutlinedButton(
+              onPressed: startLocationService,
+              child: const Text(
+                'Moin'
+              ),
+            ),
             Text(
               'Latitude: $_latitude',
               style: Theme.of(context).textTheme.headline6,
@@ -146,7 +207,7 @@ class _MyHomePageState extends State<MyHomePage> {
             Visibility(
               visible: _longitude != "",
               child:  QRCode(lang: _latitude, long: _longitude),
-            )
+            ),
             // const Text('Address: '),
             //   Text(_address),
           ],
@@ -158,5 +219,146 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.change_circle_outlined),
       ),
     );
+  }
+}
+
+class LocationCallbackHandler {
+  static Future<void> initCallback(Map<dynamic, dynamic> params) async {
+    LocationServiceRepository myLocationCallbackRepository =
+    LocationServiceRepository();
+    await myLocationCallbackRepository.init(params);
+  }
+
+  static Future<void> disposeCallback() async {
+    LocationServiceRepository myLocationCallbackRepository =
+    LocationServiceRepository();
+    await myLocationCallbackRepository.dispose();
+  }
+
+  static Future<void> callback(LocationDto locationDto) async {
+    LocationServiceRepository myLocationCallbackRepository =
+    LocationServiceRepository();
+    await myLocationCallbackRepository.callback(locationDto);
+  }
+
+  static Future<void> notificationCallback() async {
+    if (kDebugMode) {
+      print('***notificationCallback');
+    }
+  }
+}
+
+class LocationServiceRepository {
+  static final LocationServiceRepository _instance = LocationServiceRepository._();
+
+  LocationServiceRepository._();
+
+  factory LocationServiceRepository() {
+    return _instance;
+  }
+
+  static const String isolateName = 'LocatorIsolate';
+
+  int _count = -1;
+
+  Future<void> init(Map<dynamic, dynamic> params) async {
+    //TODO change logs
+    if (kDebugMode) {
+      print("***********Init callback handler");
+    }
+    if (params.containsKey('countInit')) {
+      dynamic tmpCount = params['countInit'];
+      if (tmpCount is double) {
+        _count = tmpCount.toInt();
+      } else if (tmpCount is String) {
+        _count = int.parse(tmpCount);
+      } else if (tmpCount is int) {
+        _count = tmpCount;
+      } else {
+        _count = -2;
+      }
+    } else {
+      _count = 0;
+    }
+    if (kDebugMode) {
+      print("$_count");
+    }
+    await setLogLabel("start");
+    final SendPort? send = IsolateNameServer.lookupPortByName(isolateName);
+    send?.send(null);
+  }
+
+  Future<void> dispose() async {
+    if (kDebugMode) {
+      print("***********Dispose callback handler");
+    }
+    if (kDebugMode) {
+      print("$_count");
+    }
+    await setLogLabel("end");
+    final SendPort? send = IsolateNameServer.lookupPortByName(isolateName);
+    send?.send(null);
+  }
+
+  Future<void> callback(LocationDto locationDto) async {
+    if (kDebugMode) {
+      print('$_count location in dart: ${locationDto.toString()}');
+    }
+    await setLogPosition(_count, locationDto);
+    final SendPort? send = IsolateNameServer.lookupPortByName(isolateName);
+    send?.send(locationDto);
+    _count++;
+
+  }
+
+  static Future<void> setLogLabel(String label) async {
+    final date = DateTime.now();
+    await FileManager.writeToLogFile(
+        '------------\n$label: ${formatDateLog(date)}\n------------\n');
+  }
+
+  static Future<void> setLogPosition(int count, LocationDto data) async {
+    final date = DateTime.now();
+    await FileManager.writeToLogFile(
+        '$count : ${formatDateLog(date)} --> ${formatLog(data)} --- isMocked: ${data.isMocked}\n');
+  }
+
+  static double dp(double val, int places) {
+    num mod = pow(10.0, places);
+    return ((val * mod).round().toDouble() / mod);
+  }
+
+  static String formatDateLog(DateTime date) {
+    return "${date.hour}:${date.minute}:${date.second}";
+  }
+
+  static String formatLog(LocationDto locationDto) {
+    return "${dp(locationDto.latitude, 4)} ${dp(locationDto.longitude, 4)}";
+  }
+}
+
+class FileManager {
+  static Future<void> writeToLogFile(String log) async {
+    final file = await _getTempLogFile();
+    await file.writeAsString(log, mode: FileMode.append);
+  }
+
+  static Future<String> readLogFile() async {
+    final file = await _getTempLogFile();
+    return file.readAsString();
+  }
+
+  static Future<File> _getTempLogFile() async {
+    final directory = await getTemporaryDirectory();
+    final file = File('${directory.path}/log.txt');
+    if (!await file.exists()) {
+      await file.writeAsString('');
+    }
+    return file;
+  }
+
+  static Future<void> clearLogFile() async {
+    final file = await _getTempLogFile();
+    await file.writeAsString('');
   }
 }
