001374c24012e33244175da4a824308793c255a1
Nils Friedrich <nfriedrich@pagemachine.de>
2022-12-28 20:10:04 +0100

    Add Map for older tickets

--- lib/screens/ticket_history_screen.dart
+++ lib/screens/ticket_history_screen.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:ticket_app/screens/ticket_map_screen.dart';
+import 'package:ticket_app/widgets/qr.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+import '../models/ticket.dart';
+import '../widgets/ticket_information.dart';
+import '../widgets/ticket_text.dart';
 
 class TicketHistory extends StatefulWidget {
   const TicketHistory({Key? key}) : super(key: key);
@@ -8,8 +15,108 @@ class TicketHistory extends StatefulWidget {
 }
 
 class _TicketHistoryState extends State<TicketHistory> {
+  var ticketHelper = TicketDatabaseHelper();
+  late List futureTicket;
+  bool finish = false;
+
+  _getTickets() async {
+    var list = await ticketHelper.tickets();
+    futureTicket = list;
+    setState(() {
+      finish = true;
+    });
+  }
+
+ initState() {
+   super.initState();
+    _getTickets();
+
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (finish) {
+      return ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: futureTicket.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 120,
+              child: Center(child: TicketBox(ticket: futureTicket[index])),
+            );
+          }
+      );
+    }
+
     return const Text('TicketHistory');
+
+  }
+}
+
+class TicketBox extends StatelessWidget {
+  final Ticket ticket;
+
+  const TicketBox({
+    Key? key,
+    required this.ticket,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+    onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(builder: (context)=>TicketMapScreen(ticket: ticket)));
+    },
+      child: Expanded(
+        child: Card(
+          child: Padding(
+              padding: EdgeInsets.all(8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.max,
+              children : [Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TicketText(text: 'Ticket-ID: ${ticket.id}'),
+                TicketText(text: 'Datum: ${DateTime.parse(ticket.startTime).day}.${DateTime.parse(ticket.startTime).month}.${DateTime.parse(ticket.startTime).year}'),
+                const Spacer(),
+                TicketText(text: 'Uhrzeit: ${DateTime.parse(ticket.startTime).hour}:${DateTime.parse(ticket.startTime).minute > 10 ? DateTime.parse(ticket.startTime).minute :  DateTime.parse(ticket.startTime).minute.toString().padLeft(2, '0') }'),
+                const Spacer(),
+                TicketText(text: 'Endzeit: ${DateTime.parse(ticket.endTime ?? "2012-02-27 00:00:00").hour}:${DateTime.parse(ticket.endTime ?? "2012-02-27 00:00:00").minute}'),
+              ]
+              ),
+                Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                QrImage(
+                data:
+                'Test',
+                    gapless: true,
+                    version: QrVersions.auto,
+                    size: 70,
+                    foregroundColor: Colors.black,
+                    //embeddedImage: const AssetImage('assets/images/thm.png'),
+                    //embeddedImageStyle: QrEmbeddedImageStyle(
+                    //size: const Size(80,80),
+                    //),
+                    errorStateBuilder: (cxt, err) {
+                      return const Center(
+                        child: Text(
+                          "Etwas läuft schief...",
+                          textAlign: TextAlign.center,
+                        ),
+                      );
+                    }),
+                      TicketText(text: 'Preis: 9,99€')
+                    ]
+                ),
+      ]
+          )
+        ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/screens/ticket_map_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:ticket_app/models/ticket.dart';
+import '../models/locationPoint.dart';
+
+class TicketMapScreen extends StatefulWidget {
+  const TicketMapScreen({required this.ticket,Key? key}) : super(key: key);
+
+  final Ticket ticket;
+
+  @override
+  State<TicketMapScreen> createState() => _TicketMapScreenState();
+}
+
+class _TicketMapScreenState extends State<TicketMapScreen> {
+  var locationHelper = LocationPointDatabaseHelper();
+  late List futureTicket;
+  late List locationPointsList;
+  late LatLng centerPoint;
+  late List<LatLng> route = [];
+
+  _getLocations() async {
+    int id = widget.ticket.id;
+    futureTicket = await locationHelper.locationsFromTicketid(id.toInt());
+    for (var locationPoint in futureTicket) {
+      print(locationPoint);
+
+      centerPoint = LatLng(locationPoint.latitude, locationPoint.longitude);
+      //print(centerPoint);
+      route.add(LatLng(locationPoint.latitude, locationPoint.longitude));
+    }
+  }
+
+  @override
+  void initState() {
+    _getLocations();
+    super.initState();
+    if (mounted) {
+      print('moin ' );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+        title: const Text('Reise'),
+    ),
+    body: FlutterMap(
+        options: MapOptions(
+            zoom: 12,
+            maxZoom: 13,
+            keepAlive: true,
+            center: LatLng(50.333333, 8.75)
+        ),
+        nonRotatedChildren: [
+          AttributionWidget.defaultWidget(
+            source: 'OpenStreetMap contributors',
+            onSourceTapped: null,
+          ),
+        ],
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'dev.easy-ticket.map',
+          ),
+          PolylineLayer(
+            polylineCulling: false,
+            polylines: [
+              Polyline(
+                  //points: [LatLng(50.3249, 8.7409), LatLng(50.4978, 8.6629), LatLng(50.5841, 8.6784),],
+                  points: route,
+                  color: Colors.blue,
+                  strokeWidth: 3.0),
+            ],
+          ),
+        ]),
+    );
+  }
+}
