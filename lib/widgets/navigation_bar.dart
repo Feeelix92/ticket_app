e0d13ab94d86290ee89c23606f2ac05ecfd22be1
Pascal Block <mail@pascalblock.de>
2022-12-31 14:34:18 +0100

    logout functionality

--- lib/widgets/navigation_bar.dart
+++ lib/widgets/navigation_bar.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ticket_app/models/tracking.dart';
 import 'package:ticket_app/screens/login_screen.dart';
@@ -18,6 +19,8 @@ class MyNavigationBar extends StatefulWidget {
 
 class _MyNavigationBarState extends State<MyNavigationBar> {
   int _currentIndex = 0;
+  final user = FirebaseAuth.instance.currentUser!;
+
   List<Widget> _children() => [
     TicketScreen(tracking: widget.tracking),
     MapScreen(tracking: widget.tracking),
@@ -74,6 +77,18 @@ class _MyNavigationBarState extends State<MyNavigationBar> {
                     Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => const LoginPage()))
                   },
                   child: const Text('Login'),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Align(
+                alignment: FractionalOffset.bottomCenter,
+                child: MaterialButton(
+                  onPressed: () => {
+                    FirebaseAuth.instance.signOut()
+                  },
+                  color: primaryColor,
+                  child: const Text('Logout'),
                 ),
               ),
             ),
