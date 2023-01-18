71880cfe63caa29bddda3fe34df0ca6aa4c54236
Felix Maurer <Feeelix92@users.noreply.github.com>
2023-01-18 18:43:59 +0100

    fixed position error of Expanded

--- lib/screens/ticket_history_screen.dart
+++ lib/screens/ticket_history_screen.dart
@@ -11,8 +11,8 @@ import '../models/tracking.dart';
 
 class TicketHistory extends StatefulWidget {
   final Tracking tracking;
-  const TicketHistory({Key? key, required this.tracking})
-      : super(key: key);
+
+  const TicketHistory({Key? key, required this.tracking}) : super(key: key);
 
   @override
   State<TicketHistory> createState() => _TicketHistoryState();
@@ -31,10 +31,9 @@ class _TicketHistoryState extends State<TicketHistory> {
     });
   }
 
- initState() {
-   super.initState();
+  initState() {
+    super.initState();
     _getTickets();
-
   }
 
   @override
@@ -48,16 +47,14 @@ class _TicketHistoryState extends State<TicketHistory> {
               height: 120,
               child: Center(child: TicketBox(ticket: futureTicket[index])),
             );
-          }
-      );
+          });
     }
 
     return const Text('TicketHistory');
-
   }
 }
 
-class TicketBox extends StatelessWidget {
+class TicketBox extends StatefulWidget {
   final Ticket ticket;
 
   const TicketBox({
@@ -66,60 +63,74 @@ class TicketBox extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<TicketBox> createState() => _TicketBoxState();
+}
+
+class _TicketBoxState extends State<TicketBox> {
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-    onTap: () {
-        Navigator.of(context).push(MaterialPageRoute(builder: (context)=>TicketMapScreen(ticket: ticket)));
-    },
-      child: Expanded(
-        child: Card(
-          child: Padding(
-              padding: EdgeInsets.all(8),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              mainAxisSize: MainAxisSize.max,
-              children : [Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TicketText(text: 'Ticket-ID: ${ticket.firebaseId}'),
-                TicketText(text: 'Datum: ${DateTime.parse(ticket.startTime).day}.${DateTime.parse(ticket.startTime).month}.${DateTime.parse(ticket.startTime).year}'),
-                const Spacer(),
-                TicketText(text: 'Uhrzeit: ${DateTime.parse(ticket.startTime).hour}:${DateTime.parse(ticket.startTime).minute > 10 ? DateTime.parse(ticket.startTime).minute :  DateTime.parse(ticket.startTime).minute.toString().padLeft(2, '0') }'),
-                const Spacer(),
-                TicketText(text: 'Endzeit: ${DateTime.parse(ticket.endTime ?? "2012-02-27 00:00:00").hour}:${DateTime.parse(ticket.endTime ?? "2012-02-27 00:00:00").minute}'),
-              ]
-              ),
-                Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                QrImage(
-                data:
-                '${ticket.firebaseId}',
-                    gapless: true,
-                    version: QrVersions.auto,
-                    size: 70,
-                    foregroundColor: Colors.black,
-                    //embeddedImage: const AssetImage('assets/images/thm.png'),
-                    //embeddedImageStyle: QrEmbeddedImageStyle(
-                    //size: const Size(80,80),
-                    //),
-                    errorStateBuilder: (cxt, err) {
-                      return const Center(
-                        child: Text(
-                          "Etwas läuft schief...",
-                          textAlign: TextAlign.center,
-                        ),
-                      );
-                    }),
-                      TicketText(text: 'Preis: ${ticket.ticketPrice} €')
-                    ]
-                ),
-      ]
-          )
-        ),
-        ),
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => TicketMapScreen(ticket: widget.ticket)));
+      },
+      child: Column(
+        children: [
+          Expanded(
+            child: Card(
+              child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              TicketText(
+                                  text: 'Ticket-ID: ${widget.ticket.firebaseId}'),
+                              TicketText(
+                                  text:
+                                      'Datum: ${DateTime.parse(widget.ticket.startTime).day}.${DateTime.parse(widget.ticket.startTime).month}.${DateTime.parse(widget.ticket.startTime).year}'),
+                              const Spacer(),
+                              TicketText(
+                                  text:
+                                      'Uhrzeit: ${DateTime.parse(widget.ticket.startTime).hour}:${DateTime.parse(widget.ticket.startTime).minute > 10 ? DateTime.parse(widget.ticket.startTime).minute : DateTime.parse(widget.ticket.startTime).minute.toString().padLeft(2, '0')}'),
+                              const Spacer(),
+                              TicketText(
+                                  text:
+                                      'Endzeit: ${DateTime.parse(widget.ticket.endTime ?? "2012-02-27 00:00:00").hour}:${DateTime.parse(widget.ticket.endTime ?? "2012-02-27 00:00:00").minute}'),
+                            ]),
+                        Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              QrImage(
+                                  data: '${widget.ticket.firebaseId}',
+                                  gapless: true,
+                                  version: QrVersions.auto,
+                                  size: 70,
+                                  foregroundColor: Colors.black,
+                                  //embeddedImage: const AssetImage('assets/images/thm.png'),
+                                  //embeddedImageStyle: QrEmbeddedImageStyle(
+                                  //size: const Size(80,80),
+                                  //),
+                                  errorStateBuilder: (cxt, err) {
+                                    return const Center(
+                                      child: Text(
+                                        "Etwas läuft schief...",
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    );
+                                  }),
+                              TicketText(
+                                  text: 'Preis: ${widget.ticket.ticketPrice} €')
+                            ]),
+                      ])),
+            ),
+          ),
+        ],
       ),
     );
   }
