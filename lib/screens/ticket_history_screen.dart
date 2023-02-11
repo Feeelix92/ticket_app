becb7156c652ee5f6446f44ecce4225c38fdcc36
Felix Maurer <Feeelix92@users.noreply.github.com>
2023-02-11 14:55:43 +0100

    added consumer to ticket_history_screen.dart

--- lib/screens/ticket_history_screen.dart
+++ lib/screens/ticket_history_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:ticket_app/colors.dart';
 import 'package:ticket_app/screens/ticket_map_screen.dart';
 import 'package:qr_flutter/qr_flutter.dart';
@@ -17,7 +18,20 @@ class TicketHistory extends StatefulWidget {
 
 class _TicketHistoryState extends State<TicketHistory> {
   // @TODO make billingList dynamic
-  List<String> billingList = <String>['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'];
+  List<String> billingList = <String>[
+    'Januar',
+    'Februar',
+    'März',
+    'April',
+    'Mai',
+    'Juni',
+    'Juli',
+    'August',
+    'September',
+    'Oktober',
+    'November',
+    'Dezember'
+  ];
   var ticketHelper = TicketDatabaseHelper();
   late List futureTicket;
   bool finish = false;
@@ -40,64 +54,67 @@ class _TicketHistoryState extends State<TicketHistory> {
   @override
   Widget build(BuildContext context) {
     if (finish) {
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 16.0),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const TicketText(text: 'Abrechnungzeitraum:'),
-                    // @TODO add dynamic date
-                    Expanded(child: DynamicDropdownButton(list: billingList)),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    TicketText(text: 'Abrechnungsbetrag:'),
-                    // @TODO add dynamic amount
-                    TicketText(text: '0.00 €'),
-                  ],
-                ),
-              ],
+      return Consumer<Tracking>(builder: (context, trackingService, child) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 16.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const TicketText(text: 'Abrechnungzeitraum:'),
+                      // @TODO add dynamic date
+                      Expanded(child: DynamicDropdownButton(list: billingList)),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      TicketText(text: 'Abrechnungsbetrag:'),
+                      // @TODO add dynamic amount
+                      TicketText(text: '0.00 €'),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-          Expanded(
-              child: ListView.builder(
-                  padding: const EdgeInsets.all(8.0),
-                  itemCount: futureTicket.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    if(futureTicket[index].ticketPrice != null){
-                      visibilityController = true;
-                      return Visibility(
-                        visible: visibilityController,
-                        child: FractionallySizedBox(
-
-                          child: Center(child: TicketBox(ticket: futureTicket[index])),
-                        ),
-                      );
-                    }else{
-                      visibilityController = false;
-                      return Visibility(
-                        visible: visibilityController,
-                        child: FractionallySizedBox(
-                          child: Center(child: TicketBox(ticket: futureTicket[index])),
-                        ),
-                      );
-                    }
-                  })
-          ),
-        ],
-      );
+            Expanded(
+                child: ListView.builder(
+                    padding: const EdgeInsets.all(8.0),
+                    itemCount: futureTicket.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      if (futureTicket[index].ticketPrice != null) {
+                        visibilityController = true;
+                        return Visibility(
+                          visible: visibilityController,
+                          child: FractionallySizedBox(
+                            child: Center(
+                                child: TicketBox(ticket: futureTicket[index])),
+                          ),
+                        );
+                      } else {
+                        visibilityController = false;
+                        return Visibility(
+                          visible: visibilityController,
+                          child: FractionallySizedBox(
+                            child: Center(
+                                child: TicketBox(ticket: futureTicket[index])),
+                          ),
+                        );
+                      }
+                    })),
+          ],
+        );
+      });
     }
     return Center(
         child: CircularProgressIndicator(
-          color: secondaryColor,
-        ));
+      color: secondaryColor,
+    ));
   }
 }
 
@@ -121,7 +138,7 @@ class _TicketBoxState extends State<TicketBox> {
         Navigator.of(context).push(MaterialPageRoute(
             builder: (context) => TicketMapScreen(ticket: widget.ticket)));
       },
-    child: Card(
+      child: Card(
         shape: RoundedRectangleBorder(
           side: BorderSide(
             color: secondaryColor,
@@ -132,131 +149,102 @@ class _TicketBoxState extends State<TicketBox> {
           padding: const EdgeInsets.all(16.0),
           child: Row(
               mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children:[
-              Expanded(
-              flex: 5,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: Row(children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                                 'Ticket-ID: ${widget.ticket.firebaseId}',
-                            softWrap:true,
-                            ),
-                            TicketText(
-                                text:
-                                'Datum: ${DateTime
-                                    .parse(widget.ticket.startTime)
-                                    .day}.${DateTime
-                                    .parse(widget.ticket.startTime)
-                                    .month}.${DateTime
-                                    .parse(widget.ticket.startTime)
-                                    .year}'),
-                          ],
-                        ),
-                      ),
-                    ]),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: Row(children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                                'Startbahnhof: ${widget.ticket.startStation}',
-                              softWrap:true,),
-                            TicketText(
-                                text:
-                                'Startzeit: ${DateTime
-                                    .parse(widget.ticket.startTime)
-                                    .hour}:${DateTime
-                                    .parse(widget.ticket.startTime)
-                                    .minute > 10 ? DateTime
-                                    .parse(widget.ticket.startTime)
-                                    .minute : DateTime
-                                    .parse(widget.ticket.startTime)
-                                    .minute
-                                    .toString()
-                                    .padLeft(2, '0')}'),
-                          ],
-                        ),
-                      ),
-                    ]),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                                'Endbahnhof: ${widget.ticket.endStation}',
-                              softWrap:true,),
-                            TicketText(
-                                text:
-                                'Endzeit: ${DateTime
-                                    .parse(
-                                    widget.ticket.endTime ?? "2012-02-27 00:00:00")
-                                    .hour}:${DateTime
-                                    .parse(
-                                    widget.ticket.endTime ?? "2012-02-27 00:00:00")
-                                    .minute > 10 ? DateTime
-                                    .parse(
-                                    widget.ticket.endTime ?? "2012-02-27 00:00:00")
-                                    .minute : DateTime
-                                    .parse(
-                                    widget.ticket.endTime ?? "2012-02-27 00:00:00")
-                                    .minute
-                                    .toString()
-                                    .padLeft(2, '0')}'),
-                          ],
-                        ),
-                      ),
-                    ]),
-                  ),
-                  ]),),
-
-                  Expanded(
-                    flex: 2,
-                    child:
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          QrImage(
-                              data: '${widget.ticket.firebaseId}',
-                              gapless: true,
-                              version: QrVersions.auto,
-                              size: 100,
-                              foregroundColor: Colors.black,
-                              errorStateBuilder: (cxt, err) {
-                                return const Center(
-                                  child: Text(
-                                    "Etwas läuft schief...",
-                                    textAlign: TextAlign.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 8.0),
+                          child: Row(children: [
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Ticket-ID: ${widget.ticket.firebaseId}',
+                                    softWrap: true,
                                   ),
-                                );
-                              }),
-
-                          TicketText(text: 'Preis: ${widget.ticket.ticketPrice} €')
-                        ],
-                      ),
-                ),]
-              ),
-            ),
-          ),
-        );
+                                  TicketText(
+                                      text:
+                                          'Datum: ${DateTime.parse(widget.ticket.startTime).day}.${DateTime.parse(widget.ticket.startTime).month}.${DateTime.parse(widget.ticket.startTime).year}'),
+                                ],
+                              ),
+                            ),
+                          ]),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 8.0),
+                          child: Row(children: [
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Startbahnhof: ${widget.ticket.startStation}',
+                                    softWrap: true,
+                                  ),
+                                  TicketText(
+                                      text:
+                                          'Startzeit: ${DateTime.parse(widget.ticket.startTime).hour}:${DateTime.parse(widget.ticket.startTime).minute > 10 ? DateTime.parse(widget.ticket.startTime).minute : DateTime.parse(widget.ticket.startTime).minute.toString().padLeft(2, '0')}'),
+                                ],
+                              ),
+                            ),
+                          ]),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 8.0),
+                          child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Endbahnhof: ${widget.ticket.endStation}',
+                                        softWrap: true,
+                                      ),
+                                      TicketText(
+                                          text:
+                                              'Endzeit: ${DateTime.parse(widget.ticket.endTime ?? "2012-02-27 00:00:00").hour}:${DateTime.parse(widget.ticket.endTime ?? "2012-02-27 00:00:00").minute > 10 ? DateTime.parse(widget.ticket.endTime ?? "2012-02-27 00:00:00").minute : DateTime.parse(widget.ticket.endTime ?? "2012-02-27 00:00:00").minute.toString().padLeft(2, '0')}'),
+                                    ],
+                                  ),
+                                ),
+                              ]),
+                        ),
+                      ]),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      QrImage(
+                          data: '${widget.ticket.firebaseId}',
+                          gapless: true,
+                          version: QrVersions.auto,
+                          size: 100,
+                          foregroundColor: Colors.black,
+                          errorStateBuilder: (cxt, err) {
+                            return const Center(
+                              child: Text(
+                                "Etwas läuft schief...",
+                                textAlign: TextAlign.center,
+                              ),
+                            );
+                          }),
+                      TicketText(text: 'Preis: ${widget.ticket.ticketPrice} €')
+                    ],
+                  ),
+                ),
+              ]),
+        ),
+      ),
+    );
   }
 }
